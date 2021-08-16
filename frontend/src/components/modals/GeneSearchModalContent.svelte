d904b89c2285998db98ead4f0e2c143acd65561e
Patrick König <35546377+patrick-koenig@users.noreply.github.com>
2021-08-16 10:31:10 +0200

    Improvement: Included MaterialUI icons as SVG

--- frontend/src/components/modals/GeneSearchModalContent.svelte
+++ frontend/src/components/modals/GeneSearchModalContent.svelte
@@ -151,7 +151,12 @@ const datatableSettings = {
    
     <div style="position: relative; float:left;">
         <input type="text" bind:value={query} placeholder="The ID and the description can be searched..." style="font-size: 1rem; padding-left: 35px; width: 25rem;" class="divbrowse-form-control">
-        <span class="material-icons" style="position: absolute; top: 9px; left: 6px;">search</span>
+        <!--<span class="material-icons" style="position: absolute; top: 9px; left: 6px;">search</span>-->
+        <div style="position: absolute; top: 9px; left: 6px;">
+            <svg style="width:24px;height:24px" viewBox="0 0 24 24">
+            <path fill="currentColor" d="M9.5,3A6.5,6.5 0 0,1 16,9.5C16,11.11 15.41,12.59 14.44,13.73L14.71,14H15.5L20.5,19L19,20.5L14,15.5V14.71L13.73,14.44C12.59,15.41 11.11,16 9.5,16A6.5,6.5 0 0,1 3,9.5A6.5,6.5 0 0,1 9.5,3M9.5,5C7,5 5,7 5,9.5C5,12 7,14 9.5,14C12,14 14,12 14,9.5C14,7 12,5 9.5,5Z" />
+            </svg>
+        </div>
 
         {#if showLoadingAnimation}
             <div style="position: absolute; top: 6px; left: 25.7rem;">
