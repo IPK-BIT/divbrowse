e1d0cd2c0381938075f155b382a30d0c056a004a
Patrick König <35546377+patrick-koenig@users.noreply.github.com>
2022-11-09 11:07:39 +0100

    Update __init__.py

--- divbrowse/__init__.py
+++ divbrowse/__init__.py
@@ -3,5 +3,10 @@ __version__ = _version
 
 import sys
 import logging
+
+
 logging.basicConfig(stream=sys.stderr, level=logging.INFO)
 log = logging.getLogger(__name__)
+
+
+logging.getLogger('numexpr').setLevel(logging.WARNING)
